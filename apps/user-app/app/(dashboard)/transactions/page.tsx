a5f52a13ef57d2bb8cee0c81eb24fa85eb058507
Pintu kumar <pintu1012kumar@gmail.com>
2025-03-20 02:10:19 +0530

    remove transaction page

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,35 +1,37 @@
-import { getServerSession } from "next-auth";
-import { OnRampTransactions } from "../../../components/OnRampTransactions";
-import { authOptions } from "../../lib/auth";
-import prisma from "@repo/db/client";
+// import { getServerSession } from "next-auth";
+// import { OnRampTransactions } from "../../../components/OnRampTransactions";
+// import { authOptions } from "../../lib/auth";
+// import prisma from "@repo/db/client";
 
-async function getOnRampTransactions() {
-    const session = await getServerSession(authOptions);
+// async function getOnRampTransactions() {
+//     const session = await getServerSession(authOptions);
     
-    if (!session?.user?.id) return []; // Prevent errors if session is undefined
+//     if (!session?.user?.id) return []; // Prevent errors if session is undefined
 
-    const txns = await prisma.onRampTransaction.findMany({
-        where: {
-            userId: Number(session.user.id) // Ensure userId is a number
-        }
-    });
+//     const txns = await prisma.onRampTransaction.findMany({
+//         where: {
+//             userId: Number(session.user.id) // Ensure userId is a number
+//         }
+//     });
 
-    return txns.map(t => ({
-        time: t.startTime,
-        amount: t.amount,
-        status: t.status,
-        provider: t.provider
-    }));
-}
+//     return txns.map(t => ({
+//         time: t.startTime,
+//         amount: t.amount,
+//         status: t.status,
+//         provider: t.provider
+//     }));
+// }
 
 export default async function TransactionsPage() {
-    const transactions = await getOnRampTransactions(); // Await inside an async component
+    // const transactions = await getOnRampTransactions(); // Await inside an async component
 
     return (
         <div className="w-full h-screen flex justify-center items-center">
-            <div className="pt-4">
+            {/* <div className="pt-4">
                 <OnRampTransactions transactions={transactions} />
-            </div>
+            </div> */}
+            Dashboard
         </div>
+
     );
 }
